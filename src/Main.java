423942213ab91187747e56ef5a3f4086535edf83
Charlie Hill <157416801+chill6363@users.noreply.github.com>
2024-10-12 22:56:34 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int inputNum = 5;
+        int moduloinputNum = inputNum % 2;
+
+        if(moduloinputNum == 0){
+            System.out.println("The input value is Even.");
+        }
+        else{
+            System.out.println("The input value is Odd.");
+        }
+    }
+}
